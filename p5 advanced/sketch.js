89cb6246215c27b81f092b3d53ac9e3ac33285c3
silviaandreoletti <silvia.andreoletti@mail.polimi.it>
2024-10-10 09:26:56 +0200

    modifica disegno casette l2

--- p5 advanced/sketch.js
+++ p5 advanced/sketch.js
@@ -55,25 +55,25 @@ function draw() {
   stroke("yellow");
   strokeWeight(20);
   
-  let xcasa = 10;
+  let xcasa = 30;
   let ycasa = 20;
-  let step = 50; // Distanza tra le casette
+  let step = 1; // Distanza tra le casette
   
-  for (let i = 0; i < 5; i++) { // Disegna casette
-    let x = xcasa + i * passo * 70; // Posizione orizzontale delle casette
-    let y = ycasa + 140;           // Posizione verticale delle casette
+  for (let i = 0; i < 5; i++) { // Disegno casette
+    let x = xcasa + i * step * 60; // Posizione orizzontale delle casette
+    let y = ycasa + 150;           // Posizione verticale delle casette
     
     // Disegna il rettangolo (corpo della casa)
     stroke("brown");
-    fill(200, 100, 100); // Colore del corpo della casa
+    fill(300, 100, 100); // Colore del corpo della casa
     strokeWeight(3);
     rect(x, y, 50, 50); // Rettangolo della casa (x, y, larghezza, altezza)
     
-    // Disegna il tetto
+    // Disegno il tetto
     stroke("orange");
-    fill(150, 50, 50); // Colore 
+    fill("brown"); 
     strokeWeight(3);
-    triangle(x - 10, y, x + 25, y - 40, x + 60, y); // Triangolo del tetto (coordinate dei vertici)
-  }
+    triangle(x - 10, y, x + 25, y - 40, x + 60, y); //Coordinate vertici x1, y1, x2, y2, x3, y3
+  }  
 }
 
